bfe3db41c28ea236e87be2757549f402801da391
Renato Xavier <rnatu91@gmail.com>
2020-08-05 18:58:01 -0300

    Projeto website Proffy - Iniciando com o JavaScript
    Iniciado a parte de Javascript efetuando um clone de schedule-item ao clickar no botão + Novo Horário

---
+++ scripts/addFields.js
@@ -0,0 +1,23 @@
+// Procurar o botão
+document.querySelector('#add-time')
+
+    // Quando clicar no botão
+    .addEventListener('click', cloneField)
+
+// Executar uma ação
+function cloneField() {
+    // Duplicar os campos (selecionando qual campo)
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true);
+
+    //limpar os campos (quais campos)
+    const fields = newFieldContainer.querySelectorAll('input');
+
+    //limpando os valores dos inputs 0 e 1 (campos)
+    fields.forEach((idx) =>{
+        idx.value = "";
+    })
+
+    // Colocar na página (onde colocar)
+    document.querySelector('#schedule-items').appendChild(newFieldContainer);
+
+}
