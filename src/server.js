9882ffe8df45c8baa763eec94544a7f7446c7338
Renato Xavier <rnatu91@gmail.com>
2020-08-07 22:02:27 -0300

    Projeto website Proffy - Finalizado
    Realizada a finalização do projeto com integração total ao banco de dados

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const server = express();
 
-const { pageLanding, pageStudy, pageGiveClasses } = require('./pages')
+const { pageLanding, pageStudy, pageGiveClasses, saveClasses } = require('./pages')
 
 // configurar nunjucks
 const nunjucks = require('nunjucks');
@@ -13,12 +13,18 @@ nunjucks.configure('src/views', {
 
 // Inicio e configuração do servidor
 server
-    //configurar arquivos estáticos (css, scripts, imagens)
-    .use(express.static("public"))
-    // rotas da aplicação
-    .get('/', pageLanding)
-    .get('/study', pageStudy)
-    .get('/give-classes', pageGiveClasses)
-    // start do servidor
-    .listen(5500);
+//receber os dados do req.body
+.use(express.urlencoded({ extended: true }))
+
+//configurar arquivos estáticos (css, scripts, imagens)
+.use(express.static("public"))
+
+// rotas da aplicação
+.get('/', pageLanding)
+.get('/study', pageStudy)
+.get('/give-classes', pageGiveClasses)
+.post('/save-classes', saveClasses)
+
+// start do servidor
+.listen(5500);
 
